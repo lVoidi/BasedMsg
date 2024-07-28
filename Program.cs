d2ce38cba98a486615c7243658fba0d4a344b6b4
lvoidi <void@localhost.localdomain>
2024-07-28 14:41:33 -0600

    Added terminal argument parsing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class OpenClient
+{
+    static void Main(string[] args)
+    {
+	int port = 8080;
+	if (args[0] == "port"){
+		try 
+		{
+			port = Int32.Parse(args[1]);
+		}
+		catch (FormatException) 
+		{
+			Console.WriteLine("Especificar un puerto válido");
+			Environment.Exit(0);
+		}
+	} else {
+		Console.WriteLine("No se especificó el puerto, saliendo...");
+		Environment.Exit(0);
+	}
+
+	Console.WriteLine("[OK] Abriendo puerto {0}", port);
+
+    }
+}
